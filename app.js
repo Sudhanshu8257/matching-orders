c963a0dfd5f451327b2fce19744b6b595c088bf3
Sudhanshu <lohanasudhanshu8257@gmail.com>
2024-09-02 14:03:48 +0530

    added config for cors

--- app.js
+++ app.js
@@ -3,12 +3,12 @@ import router from "./routes/order-routes.js";
 import cors from "cors";
 
 const app = express();
-
+const corsConfig = {
+  credentials: true,
+  origin: ["http://localhost:3000", "https://orders-chart.vercel.app/"],
+};
 app.use(express.json());
-app.use(cors({
-  origin: '*', // Replace with your frontend URL
-  credentials: true
-}));
+app.use(cors(corsConfig));
 
 app.use("/api/v1", router);
 export default app;
