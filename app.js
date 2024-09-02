5cbf4f70ae6c74cbe77d70a6b0ef6aadb8957573
Sudhanshu <lohanasudhanshu8257@gmail.com>
2024-09-02 17:01:27 +0530

    changed uri

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ import cors from "cors";
 const app = express();
 const corsConfig = {
   credentials: true,
-  origin: ["http://localhost:3000", "https://orders-chart.vercel.app/"],
+  origin: ["http://localhost:3000", "https://orders-chart.vercel.app"],
 };
 app.use(express.json());
 app.use(cors(corsConfig));
